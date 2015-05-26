df58fd96be2ee7ecddaf83d15be524900ae8174e
Paweł Dawczak <pawel.dawczak@gmail.com>
2015-05-26 11:58:11 +0100

    Create new Item

--- features/items.feature
+++ features/items.feature
@@ -9,3 +9,9 @@ Feature: Items
     Then I should see item "Butter"
     And I should see item "Potatoes"
     And I should see item "Juice"
+
+  Scenario: Adding new Item
+    Given I am at the home page
+    When I fill in items name form field with "Beer"
+    And I submit new item form
+    Then I should see item "Beer"
--- features/step_definitions/item_steps.rb
+++ features/step_definitions/item_steps.rb
@@ -5,3 +5,13 @@ end
 Then(/^I should see item "(.*?)"$/) do |item_name|
   expect(page).to have_content item_name
 end
+
+When(/^I fill in items name form field with "(.*?)"$/) do |item_name|
+  within("#new_item") do
+    fill_in "Item name", with: item_name
+  end
+end
+
+When(/^I submit new item form$/) do
+  click_button "Add"
+end
